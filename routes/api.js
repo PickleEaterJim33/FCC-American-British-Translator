23d1a3bd64f00dc0feef01267375c26c2816b6b2
Jan Mlinarić <jan.mlinaric@gmail.com>
2022-11-22 04:01:45 +0100

    feat: complete ./routes/api.js

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,25 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      const text = req.body.text;
+      const locale = req.body.locale;
+
+      if (typeof text === 'undefined' || text === null
+      || typeof locale === 'undefined' || locale === null) {
+        return res.send({ error: 'Required field(s) missing' });
+      }
+
+      if (text === '') return res.send({ error: 'No text to translate' });
+
+      if (locale !== 'american-to-british' && locale !== 'british-to-american') {
+        return res.send({ error: 'Invalid value for locale field' });
+      }
+
+      const result = translator.translate(text, locale);
+      if (result.text === result.translation) {
+        result.translation = 'Everything looks good to me!';
+      }
+
+      return res.send(result);
     });
 };
